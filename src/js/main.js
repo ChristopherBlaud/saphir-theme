9fcc03d2a539952ca25c7af67ca83f48efe91a8d
ChristopherBlaud <contact@cblaud.com>
2024-04-14 18:32:16 +0200

    added sliders & gsap

--- src/js/main.js
+++ src/js/main.js
@@ -11,21 +11,32 @@ gsap.registerPlugin(ScrollTrigger);
     if (animatedModules) {
         animatedModules.forEach(module => {
             const YPos = parseInt(module.dataset.yPosition, 10) || 0,
+                XPos = parseInt(module.dataset.xPosition, 10) || 0,
+                scale = parseInt(module.dataset.scale, 10) || 1,
+                rotate = parseInt(module.dataset.rotate, 10) || 0,
+                skewX = parseInt(module.dataset.skewX, 10) || 0,
+                skewY = parseInt(module.dataset.skewY, 10) || 0,
                 opacity = (parseInt(module.dataset.opacity, 10) / 100) || 1,
-                delay = parseInt(module.dataset.animationDelay, 10) || 0;
+                delay = parseInt(module.dataset.animationDelay, 10) || 0,
+                bindOnScroll = JSON.parse(module.dataset.bindOnScroll);
 
             module.closest('.dnd-section').style.overflow = 'hidden';
 
             gsap.from(module, {
+                x: XPos,
                 y: YPos,
+                scale: scale,
+                rotate: rotate,
+                skewX: skewX,
+                skewY: skewY,
                 opacity: opacity,
                 scrollTrigger: {
-                    trigger: module,
+                    trigger: module.parentNode,
                     start: 'top bottom',
                     end: `center bottom-=${delay}`,
-                    scrub: true
+                    scrub: bindOnScroll
                 }
-            })
+            });
         });
-    } 
+    }; 
 })();
