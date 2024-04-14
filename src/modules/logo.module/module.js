1bfc922452b16b857294b4e69ff43f149b2fb614
ChristopherBlaud <contact@cblaud.com>
2024-04-14 10:53:31 +0200

    added params for logos-slider module

--- src/modules/logo.module/module.js
+++ src/modules/logo.module/module.js
@@ -2,17 +2,38 @@ document.addEventListener('DOMContentLoaded', () => {
     const logosSliders = document.querySelectorAll('.logo-slider');
 
     logosSliders && logosSliders.forEach(slider => {
-        const itemsSpacingXL = slider.dataset.itemsSpacingXl;
+        const itemsSpacingXL = slider.dataset.itemsSpacingXl,
+            itemsSpacingMD = slider.dataset.itemsSpacingMd,
+            itemsSpacingXS = slider.dataset.itemsSpacingXs,
+            slidesPerViewXL = slider.dataset.slidesPerViewXl,
+            slidesPerViewMD = slider.dataset.slidesPerViewMd,
+            slidesPerViewXS = slider.dataset.slidesPerViewXs,
+            speedXL = slider.dataset.sliderAnimationDurationXl,
+            speedMD = slider.dataset.sliderAnimationDurationMd,
+            speedXS = slider.dataset.sliderAnimationDurationXs;
+        
 
         const swiper = new Swiper(slider, {
             direction: 'horizontal',
-            slidesPerView: 4,
-            spaceBetween: itemsSpacingXL, 
+            slidesPerView: slidesPerViewXS,
+            spaceBetween: itemsSpacingXS, 
             loop: true,
-            speed: 5000,
+            speed: speedXS,
             autoplay: {
-                delay: 0,
+                delay: 0
+            },
+            breakpoints: {
+                1024: {
+                    slidesPerView: slidesPerViewXL,
+                    spaceBetween: itemsSpacingXL,
+                    speed: speedXL
+                },
+                768: {
+                    slidesPerView: slidesPerViewMD,
+                    spaceBetween: itemsSpacingMD,
+                    speed: speedMD
+                }
             }            
-        })
+        });
     });
 });
