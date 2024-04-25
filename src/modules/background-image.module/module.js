04655d6edad06f171cf111218aa60027cb63bad8
ChristopherBlaud <contact@cblaud.com>
2024-04-25 19:39:21 +0200

    start creating background-image module

---
+++ src/modules/background-image.module/module.js
@@ -0,0 +1,28 @@
+const fetchSVGContent = async (url, container) => {
+    try {
+        const response = await fetch(url);
+
+        if (!response.ok) {
+            throw new Error('Error occurred during file recovery');
+        } ;
+
+        const result =  await response.text();
+        container.innerHTML = result;
+    } catch (error) {
+        console.error(error);
+    };
+};
+
+(() => {
+    const imageContainer = document.querySelectorAll('.background-image__inner');
+
+    imageContainer && imageContainer.forEach(container => {
+        const url = container.dataset.src
+
+        if (url.endsWith('.svg')) {
+            fetchSVGContent(url, container);
+        } else {
+            container.textContent = "Error. Only the svg format is supported."
+        } 
+    });
+})();
